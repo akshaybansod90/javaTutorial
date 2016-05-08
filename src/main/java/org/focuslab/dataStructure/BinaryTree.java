ed4774a8f7156b0a3df5f86b525bcc096b369867
akshaybansod90 <akshaybansod90@gmail.com>
2016-05-08 13:31:38 +0530

    binary tree updates

--- src/main/java/org/focuslab/dataStructure/BinaryTree.java
+++ src/main/java/org/focuslab/dataStructure/BinaryTree.java
@@ -12,36 +12,6 @@ public class BinaryTree<T> {
         this.root = root;
     }
 
-    public static class Node<T>{
-        T data ;
-        Node<T> left;
-        Node<T> right;
-
-        public T getData() {
-            return data;
-        }
-
-        public void setData(T data) {
-            this.data = data;
-        }
-
-        public Node<T> getLeft() {
-            return left;
-        }
-
-        public void setLeft(Node<T> left) {
-            this.left = left;
-        }
-
-        public Node<T> getRight() {
-            return right;
-        }
-
-        public void setRight(Node<T> right) {
-            this.right = right;
-        }
-    }
-
     public ArrayList<T> preorderTraversal(){
         ArrayList<T> preorderResult = new ArrayList<T>();
         preorder(this.root,preorderResult);
@@ -70,6 +40,51 @@ public class BinaryTree<T> {
         inorder(root.left,inorderResult);
         inorderResult.add(root.getData());
         inorder(root.right,inorderResult);
+    }
+
+    public ArrayList<T> postorderTraversal() {
+        ArrayList<T> postorderResult = new ArrayList<T>();
+        postorder(this.root, postorderResult);
+        return postorderResult;
+    }
+
+    private void postorder(Node<T> root, ArrayList<T> postorderResult) {
+        if (root == null) {
+            return;
+        }
+        postorder(root.left, postorderResult);
+        postorderResult.add(root.getData());
+        postorder(root.right, postorderResult);
+    }
+
+    public static class Node<T> {
+        T data;
+        Node<T> left;
+        Node<T> right;
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node<T> getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node<T> left) {
+            this.left = left;
+        }
+
+        public Node<T> getRight() {
+            return right;
+        }
+
+        public void setRight(Node<T> right) {
+            this.right = right;
+        }
     }
 
 }
